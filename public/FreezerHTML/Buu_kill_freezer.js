5fe8330a7d01f9f385541f54e735a86dc588a3d7
Micka-dej <33249171+Micka-dej@users.noreply.github.com>
2018-06-07 00:01:26 +0200

    Finishing HTML/CSS

---
+++ public/FreezerHTML/Buu_kill_freezer.js
@@ -0,0 +1,9 @@
+
+  var buu = document.querySelector('.sectionThirdFreezer-containerBuu-container4');
+  var text = document.querySelector('.sectionThirdFreezer-containerBuu-container4-description');
+
+
+window.addEventListener('load',function(){
+  setTimeout(function(){text.style.display='none';
+  buu.style.background="no-repeat center/100% 100% url('../assets/img/Buu_gif.gif')"; },5000);
+})
