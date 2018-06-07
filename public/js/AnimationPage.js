77b6dee6047f1b41fc1b4337448fd848b485dc8f
Micka-dej <33249171+Micka-dej@users.noreply.github.com>
2018-06-07 11:44:56 +0200

    Finish update

---
+++ public/js/AnimationPage.js
@@ -0,0 +1,27 @@
+// var anim = {
+//   vegetaSection1: document.querySelector('.sectionVegetaKillGoku'),
+//   vegetaSection2: document.querySelector('.sectionVegetaKillGoku-vegetachie'),
+//   vegetaSection3: document.querySelector('.sectionVegetaKillGoku-chichi'),
+//   sectionRetry: document.querySelector('.sectionRetry'),
+// }
+//
+//   function changeSection(){
+//   setTimeout(function(){
+//     anim.vegetaSection1.style.animationName='rotateOutUpRight';
+//     anim.vegetaSection1.style.animationDuration='2s';
+//   },4500);
+//
+//   setTimeout(function(){
+//     anim.vegetaSection2.style.animationName='fadeInUp';
+//     anim.vegetaSection2.style.animationDuration='2s';
+//   },4800);
+//
+//   setTimeout(function(){
+//     anim.vegetaSection1.style.display='none';
+//     anim.vegetaSection2.style.display='block';
+//   },5000);
+// }
+//
+//   window.addEventListener('load',function(){
+//     changeSection();
+//   })
