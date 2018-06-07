d670e46215de4272159aeeb83237a762410dcbd2
Micka-dej <33249171+Micka-dej@users.noreply.github.com>
2018-06-07 03:48:25 +0200

    update

---
+++ public/js/TitleChoiceGokuSecondPage.js
@@ -0,0 +1,6 @@
+var text = document.querySelectorAll('.ChoiceGoku-title');
+
+window.addEventListener('load',function(){
+text[0].innerHTML = GokuDataSecondChoice[0].content;
+text[1].innerHTML = GokuDataSecondChoice[1].content;
+})
---
+++ public/js/data.js
@@ -0,0 +1,66 @@
+var GokuData = [{
+    'content': 'Goku arrive en retard'
+  },
+  {
+    'content': 'Goku acheve vegeta'
+  },
+
+  {
+    'content': 'Goku laisse krillin sauver vegeta'
+  },
+];
+
+var GokuDataSecondChoice =[{
+    'content': 'Goku accepte l\'aide de son gohan'
+  },
+
+  {
+    'content': 'Goku refuse son aide'
+  },
+];
+
+var VegetaData = [{
+    'content': 'Vegeta vs Goku'
+  },
+  {
+    'content': 'Vegeta tue Goku'
+  },
+
+  {
+    'content': 'Rencontre bulma'
+  },
+];
+
+var VegetaDataSecondChoice = [
+  {
+    'content': 'Vegeta gagne et ...'
+  },
+
+  {
+    'content': 'Vegeta y croit et ...'
+  },
+];
+
+var FreezerData = [{
+    'content': 'Freezer trouve un alliee et ...'
+  },
+
+  {
+    'content': 'Freezer recherche les dragonballs'
+  },
+
+  {
+    'content': 'Freezer arrive sur terre'
+  },
+
+];
+
+var FreezerDataSecondChoice = [
+  {
+    'content': 'Freezer tue goku'
+  },
+
+  {
+    'content': 'Freezer tue krillin'
+  },
+];
